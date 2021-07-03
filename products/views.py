96e6adef8a19651ad1b3cdefb27a991bcaedaeb2
Brianconn71 <Brian.connolly71@gmail.com>
2021-07-03 15:29:41 +0000

    add: added products html and templaste to display products on site

--- products/views.py
+++ products/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import Product
 
-# Create your views here.
+
+def all_products(request):
+    """ This view returns all the products on the site with sorting and search queries too."""
+
+    products = Product.objects.all()
+
+    context = {
+        'products': products,
+    }
+
+    return render(request, 'products/products.html', context)
