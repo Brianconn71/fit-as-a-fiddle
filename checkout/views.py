4eb9ec2ce02a67e78e3cf5326ede97202a0744f5
Brianconn71 <Brian.connolly71@gmail.com>
2021-07-12 11:12:30 +0000

    add: added stripe elements and stripe payments top checkout page

--- checkout/views.py
+++ checkout/views.py
@@ -15,6 +15,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51J41AKJlYwbBfggKoPYVxYOOHm4Q78kltZxae5z3BZlP7epmydxbwYprNB3VeMRoaJs4Hy1ew15TgC0azwJuqRf9000QM0ExHG',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
