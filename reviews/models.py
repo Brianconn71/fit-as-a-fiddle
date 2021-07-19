f090ac0fabf6d554fe59993010ed0807fea1cdd4
Brianconn71 <Brian.connolly71@gmail.com>
2021-07-19 10:55:30 +0000

    add: set up review model in models and made migrsations and migrated to backend

--- reviews/models.py
+++ reviews/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
+from products.models import Product
 
-# Create your models here.
+
+# Review system model
+class Review(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='reviews')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='reviews')
+    title = models.CharField(max_length=254, null=False, blank=False)
+    comment = models.TextField(max_length=1000, null=False, blank=False)
+    rating = models.DecimalField(default=0, max_digits=2, decimal_places=1, null=False, blank=False)
+    created = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.user.username
