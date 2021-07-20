afbaa17eb38fcf7040bdaa7cb479cd5a69946595
Brianconn71 <Brian.connolly71@gmail.com>
2021-07-20 19:50:01 +0000

    add: finally got the reviews to work properly on product details page, had forgotten about urls.py in fit_as_a_fiddle

--- reviews/models.py
+++ reviews/models.py
@@ -5,11 +5,18 @@ from products.models import Product
 
 # Review system model
 class Review(models.Model):
+    RATING_CHOICES = (
+        (1,'1'),
+        (2,'2'),
+        (3,'3'),
+        (4,'4'),
+        (5,'5'),
+    )
     product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='reviews')
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='reviews')
     title = models.CharField(max_length=254, null=False, blank=False)
     comment = models.TextField(max_length=1000, null=False, blank=False)
-    rating = models.DecimalField(default=0, max_digits=2, decimal_places=1, null=False, blank=False)
+    rating = models.IntegerField(choices=RATING_CHOICES, null=False, blank=False)
     created = models.DateField(auto_now_add=True)
 
     def __str__(self):
