6d6d4dc9efaefb2590e2c9d1b53377a827cf0f6f
Brianconn71 <Brian.connolly71@gmail.com>
2021-07-23 15:13:47 +0000

    edit: removed secret key from local environment and changed debud

--- fit_as_a_fiddle/settings.py
+++ fit_as_a_fiddle/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-g$_2-k3v73q3hdtd5$yxy$-^p^nqn%bkrpegcy6r+zwq5!wn%s'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['ms4-fit-as-a-fiddle.herokuapp.com', 'localhost']
 
