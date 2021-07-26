fe8e9e87ac67ce81227642f4a0ca43335b0920e6
Brianconn71 <Brian.connolly71@gmail.com>
2021-07-26 21:02:31 +0000

    add: added urls and path to blog

--- blog/models.py
+++ blog/models.py
@@ -1,15 +1,21 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.template.defaultfilters import slugify
 
 
 # blog model
 class Post(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=250, null=False, blank=False)
-    slug = models.SlugField(max_length=250, null=True, blank=True)
+    slug = models.SlugField()
     intro = models.TextField(max_length=250, null=False, blank=False)
     body = models.TextField(max_length=1000, null=False, blank=False)
     date_added = models.DateTimeField(auto_now_add=True)
+
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        super(Post, self).save(*args, **kwargs)
 
     def __str__(self):
         return self.user.username
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import Post
 
-# Create your views here.
+
+def blog(request):
+    """ This view returns the blog page"""
+    posts = Post.objects.all()
+
+    template = 'blog/blog.html'
+    context = {
+        'posts': posts
+    }
+
+    return render(request, template, context)
