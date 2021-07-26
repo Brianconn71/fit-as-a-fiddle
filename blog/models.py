6f6e1521c1910f21274e9302b6b536f60e3d4e08
Brianconn71 <Brian.connolly71@gmail.com>
2021-07-26 16:07:06 +0000

    add: added migrations for blog model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+# blog model
+class Post(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=250, null=False, blank=False)
+    slug = models.SlugField(max_length=250, null=True, blank=True)
+    intro = models.TextField(max_length=250, null=False, blank=False)
+    body = models.TextField(max_length=1000, null=False, blank=False)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    class Meta: 
+        ordering = ['-date_added']
+
