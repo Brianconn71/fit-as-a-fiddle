6473ae480eda7ed98c6b05559eff4ee591eb2a98
Brianconn71 <Brian.connolly71@gmail.com>
2021-07-27 16:27:49 +0000

    add: attempting to add the ability to edit reviews

--- reviews/views.py
+++ reviews/views.py
@@ -12,9 +12,9 @@ from django.contrib import messages
 @login_required
 def add_review(request, product_id):
     product = Product.objects.get(id=product_id)
-    # if not request.user.is_authenticated:
-    #     messages.error(request, 'Sorry, only logged in members may leave a review')
-    #     return redirect(reverse('home'))
+    if not request.user.is_authenticated:
+        messages.error(request, 'Sorry, only logged in members may leave a review')
+        return redirect(reverse('home'))
 
     if request.user.is_authenticated:
         if request.method == "POST":
@@ -35,3 +35,31 @@ def add_review(request, product_id):
         messages.error(request,'Please sign in to leave a review')
         return redirect('account_login')
 
+
+@login_required
+def edit_review(request, review_id):
+    """ edits a product on the site """
+    if review.user != request,user:
+        messages.error(request, 'Sorry, you do not have the required permissions to do this')
+        return redirect(reverse('product_details', product.id))
+
+    product = get_object_or_404(Product, pk=product_id)
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES, instance=product)
+        if form.is_valid():
+            form.save()
+            messages.success(request, f'Successfully updated {product.name}!')
+            return redirect(reverse('product_details', args=[product.id]))
+        else:
+            messages.error(request, f'Failed to update {product.name}. Please ensure form is valid!')
+    else:
+        form = ProductForm(instance=product)
+        messages.info(request, f'You are editing {product.name}')
+
+    template = 'products/edit_product.html'
+    context = {
+        'form': form,
+        'product': product,
+    }
+
+    return render(request, template, context)
