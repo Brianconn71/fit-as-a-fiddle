b8985307a3f8b5c14b2d92f4010cfd7e283e0d41
Brianconn71 <Brian.connolly71@gmail.com>
2021-07-27 14:21:56 +0000

    add: added changes and migrations to the database

--- blog/models.py
+++ blog/models.py
@@ -7,7 +7,7 @@ from django.template.defaultfilters import slugify
 class Post(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=250, null=False, blank=False)
-    slug = models.SlugField()
+    slug = models.SlugField(default='')
     intro = models.TextField(max_length=250, null=False, blank=False)
     body = models.TextField(max_length=1000, null=False, blank=False)
     date_added = models.DateTimeField(auto_now_add=True)
@@ -18,7 +18,7 @@ class Post(models.Model):
         super(Post, self).save(*args, **kwargs)
 
     def __str__(self):
-        return self.user.username
+        return self.title
 
     class Meta: 
         ordering = ['-date_added']
