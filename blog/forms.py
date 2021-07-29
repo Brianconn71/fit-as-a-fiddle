eaaa6b7c5c6f51349e9a86e5a2a030ced0fe3c41
Brianconn71 <Brian.connolly71@gmail.com>
2021-07-29 20:43:14 +0000

    edit: changed the styling on some of the blog elements to make it look more visually appealing

--- blog/forms.py
+++ blog/forms.py
@@ -6,7 +6,7 @@ from .models import Post, Comment
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
-        fields = ['title', 'body']
+        fields = ['title', 'intro','body']
 
 
 class CommentForm(forms.ModelForm):
