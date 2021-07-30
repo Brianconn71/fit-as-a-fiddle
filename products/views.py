c993be6a2f10ecc37534ab398fc927f97c7fa66a
Brianconn71 <Brian.connolly71@gmail.com>
2021-07-30 20:17:58 +0000

    edit: made one change to products view and see if it fixes the issue with not returning correct category

--- products/views.py
+++ products/views.py
@@ -43,7 +43,7 @@ def all_products(request):
 
         if 'category' in request.GET:
             categories = request.GET['category'].split(',')
-            products = products.filter(category__name__in=categories)
+            products = page.filter(category__name__in=categories)
             categories = Category.objects.filter(name__in=categories)
 
         if 'search' in request.GET:
