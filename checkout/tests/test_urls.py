818582048792db1e685ff93ddc53ab4a563956ab
Brianconn71 <Brian.connolly71@gmail.com>
2021-08-06 18:54:32 +0000

    add: added automated tests for checkout urls

---
+++ checkout/tests/test_urls.py
@@ -0,0 +1,21 @@
+from django.test import SimpleTestCase
+from django.urls import reverse, resolve
+from checkout.views import cache_checkout_data, checkout, checkout_success
+
+
+class Test_urls(SimpleTestCase):
+
+    def test_cache_checkout_data_resolves(self):
+        url = reverse('cache_checkout_data')
+        print(resolve(url))
+        self.assertEquals(resolve(url).func, cache_checkout_data)
+    
+    def test_checkout_resolves(self):
+        url = reverse('checkout')
+        print(resolve(url))
+        self.assertEquals(resolve(url).func, checkout)
+    
+    def test_checkout_success_resolves(self):
+        url = reverse('checkout_success', args=[3])
+        print(resolve(url))
+        self.assertEquals(resolve(url).func, checkout_success)
