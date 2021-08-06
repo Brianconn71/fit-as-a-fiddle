ad82b3470d1aa0b34d7a82ca1b00cba899eacec1
Brianconn71 <Brian.connolly71@gmail.com>
2021-08-06 19:13:54 +0000

    add: added automated tests for profiles urls

---
+++ profiles/tests/test_urls.py
@@ -0,0 +1,16 @@
+from django.test import SimpleTestCase
+from django.urls import reverse, resolve
+from profiles.views import profile, order_history
+
+
+class Test_urls(SimpleTestCase):
+
+    def test_profile_resolves(self):
+        url = reverse('profile')
+        print(resolve(url))
+        self.assertEquals(resolve(url).func, profile)
+    
+    def test_order_history_resolves(self):
+        url = reverse('order_history', args=[2])
+        print(resolve(url))
+        self.assertEquals(resolve(url).func, order_history)
