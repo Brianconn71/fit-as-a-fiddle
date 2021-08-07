c187505b1f1dc978c8e0a9045f66bb540ce83530
Brianconn71 <Brian.connolly71@gmail.com>
2021-08-07 17:13:00 +0000

    add: added testing forms to blog app

---
+++ blog/tests/test_forms.py
@@ -0,0 +1,20 @@
+from django.test import SimpleTestCase
+from blog.forms import PostForm, CommentForm
+
+
+class TestForms(SimpleTestCase):
+
+    def test_Post_Form_valid_data(self):
+        form = PostForm(data={
+            'title': 'test title',
+            'intro': 'test intro',
+            'body': 'test'
+        })
+
+        self.assertTrue(form.is_valid())
+    
+    def test_post_form_invalid(self):
+        form = PostForm(data={})
+
+        self.assertFalse(form.is_valid())
+        self.assertEquals(len(form.errors), 2)
