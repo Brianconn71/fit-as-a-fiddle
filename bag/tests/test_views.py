64c6bcd2ea22bf5d3615014d3377cf92957dd22d
Brianconn71 <Brian.connolly71@gmail.com>
2021-08-07 15:24:19 +0000

    add: added testing urls to bag app

---
+++ bag/tests/test_views.py
@@ -0,0 +1,23 @@
+from django.test import TestCase, Client
+from django.urls import reverse
+from django.contrib.auth.models import User
+import json
+
+
+class TestViews(TestCase):
+
+    def setUp(self):
+        self.client = Client()
+        self.user = User.objects.create_user(
+            username='testuser',
+            email='test@email.com',
+            password='testpassword'
+        )
+        self.view_bag_url = reverse('view_bag')
+
+
+    def test_view_bag_GET(self):
+        response = self.client.get(self.view_bag_url)
+
+        self.assertEquals(response.status_code, 200)
+        self.assertTemplateUsed(response, 'bag/bag.html')
