357764accc7cbbfc8f1d290070ddd325dbff76fd
Brianconn71 <Brian.connolly71@gmail.com>
2021-08-07 14:32:50 +0000

    add: added basic get view for blog and checkout

--- blog/tests/test_views.py
+++ blog/tests/test_views.py
@@ -1,15 +1,48 @@
 from django.test import TestCase, Client
 from django.urls import reverse
+from django.contrib.auth.models import User
 from blog.models import Post, Comment
 import json
 
 
 class TestViews(TestCase):
 
-    def test_blog_GET(self):
-        client = Client()
+    def setUp(self):
+        self.client = Client()
+        self.user = User.objects.create_user(
+            username='testuser',
+            email='test@email.com',
+            password='testpassword'
+        )
+        self.blog_url = reverse('blog')
+        self.blog_post_url = reverse('blog_post', args=['sometext'])
+        self.sometext = Post.objects.create(
+            title='sometext',
+            user=self.user,
+        )
 
-        response = client.get(reverse('blog'))
+
+    def test_blog_GET(self):
+        response = self.client.get(self.blog_url)
 
         self.assertEquals(response.status_code, 200)
         self.assertTemplateUsed(response, 'blog/blog.html')
+    
+    def test_blog_post_GET(self):
+        response = self.client.get(self.blog_post_url)
+
+        self.assertEquals(response.status_code, 200)
+        self.assertTemplateUsed(response, 'blog/blog_post.html')
+    
+    def test_blog_post_POST_adds_new_post(self):
+        Post.objects.create(
+            title=self.title1,
+            user=self.user,
+        )
+
+        response = self.client.post(self.blog_post_url, {
+
+        })
+
+        self.assertEquals(response.status_code, 200)
+        self.assertTemplateUsed(self.title1, 'banana')
---
+++ checkout/tests/test_views.py
@@ -0,0 +1,44 @@
+from django.test import TestCase, Client
+from django.urls import reverse
+from django.contrib.auth.models import User
+from checkout.models import Order, OrderLineItem
+from django.contrib.messages import get_messages
+import json
+
+
+class TestViews(TestCase):
+
+    def setUp(self):
+        self.client = Client()
+        self.user = User.objects.create_user(
+            username='testuser',
+            email='test@email.com',
+            password='testpassword'
+        )
+        self.checkout_url = reverse('checkout')
+
+    def test_checkout_GET(self):
+        response = self.client.get(self.checkout_url)
+        self.assertEqual(response.status_code, 302)
+        messages = list(get_messages(response.wsgi_request))
+        self.assertEqual(len(messages), 1)
+        self.assertEqual(str(messages[0]),
+                         "You currently have no bag items")
+    
+    # def test_blog_post_GET(self):
+    #     response = self.client.get(self.blog_post_url)
+
+    #     self.assertEquals(response.status_code, 200)
+    #     self.assertTemplateUsed(response, 'blog/blog_post.html')
+
+    #     Post.objects.create(
+    #         title=self.title1,
+    #         user=self.user,
+    #     )
+
+    #     response = self.client.post(self.blog_post_url, {
+
+    #     })
+
+    #     self.assertEquals(response.status_code, 200)
+    #     self.assertTemplateUsed(self.title1, 'banana')
