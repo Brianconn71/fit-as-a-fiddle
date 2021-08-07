d7b0d2fff936192859fae784118bbc9c4419aa6c
Brianconn71 <Brian.connolly71@gmail.com>
2021-08-07 17:01:15 +0000

    add: added testing models to profiles app

---
+++ profiles/tests/test_models.py
@@ -0,0 +1,20 @@
+from django.test import TestCase
+from profiles.models import UserProfile
+from django.contrib.auth.models import User
+
+
+class TestModels(TestCase):
+
+    def setUp(self):
+        self.user = User.objects.create_user(
+            username='testuser',
+            password='testpassword'
+        )
+        self.profile = UserProfile.objects.get(
+            user=self.user,
+        )
+    
+    def test_userprofile_model(self):
+        self.client.login(username='testuser', password='testpassword')
+        
+        self.assertEquals(str(self.profile), 'testuser')
