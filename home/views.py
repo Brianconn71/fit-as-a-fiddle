f5dc1790063e37f9d2816168db679e11def4173a
Brianconn71 <Brian.connolly71@gmail.com>
2021-08-20 20:43:45 +0000

    add: added contact us page

--- home/views.py
+++ home/views.py
@@ -1,7 +1,32 @@
 from django.shortcuts import render
+from .forms import ContactForm
+from django.core.mail import send_mail
 
 
 def home(request):
     """ This view returns the index page"""
 
     return render(request, 'home/index.html')
+
+
+def contact(request):
+
+    if request.method == "POST":
+        form = ContactForm(request.POST)
+        if form.is_valid:
+            form.subject = request.POST['subject'],
+            form.message = request.POST['message'],
+            form.email = request.POST['email'],
+
+        send_mail(
+            form.subject,
+            form.message,
+            form.email,
+            ['brian.connolly71@gmail.com']
+        )
+        messages.success(request,
+                            "Your message has been sent.\
+                            A member of our customer service team will be in contact soon.")
+        return redirect("home_")
+
+
